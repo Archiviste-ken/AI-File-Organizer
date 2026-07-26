a19431df5e0175985c97ff789f13a3e96503e64c
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-27 01:12:36 +0530

    add organizer.py with application responsibilities and structure

---
+++ organizer.py
@@ -0,0 +1,23 @@
+# 📄 organizer.py
+
+# This is the heart of the application.
+
+# Responsibilities:
+
+# ✔ Scan Folder
+
+# ✔ Detect File Type
+
+# ✔ Create Folder
+
+# ✔ Move File
+
+# ✔ Count Files
+
+# If someone asks
+
+# "How do files get organized?"
+
+# The answer is:
+
+# organizer.py
