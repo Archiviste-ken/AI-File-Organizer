b6a2353727ee85455e410327a05eb5979aab506a
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-27 01:13:27 +0530

    add file_utils.py with utility functions for folder management and file handling

---
+++ file_utilize.py
@@ -0,0 +1,23 @@
+# 📄 file_utils.py
+
+# Think of this as a toolbox.
+
+# Responsibilities:
+
+# ✔ Check if folder exists
+
+# ✔ Create folders
+
+# ✔ Get extension
+
+# ✔ Get filename
+
+# ✔ Helper functions
+
+# Notice...
+
+# These are small reusable functions.
+
+# They don't organize anything.
+
+# They simply help.
