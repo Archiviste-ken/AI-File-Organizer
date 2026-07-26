4922ed9858f55b1c1f5ee39d473d3b6d9cf75155
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-27 01:17:44 +0530

    add config.py to centralize file extension categories for better organization

---
+++ config.py
@@ -0,0 +1,23 @@
+# 📄 config.py
+
+# Another professional practice.
+
+# Instead of writing:
+
+# IMAGE_EXTENSIONS = [".png", ".jpg", ".jpeg"]
+
+# inside multiple files...
+
+# We'll store all categories in one place.
+
+# Example:
+
+# IMAGE_EXTENSIONS = [...]
+
+# VIDEO_EXTENSIONS = [...]
+
+# MUSIC_EXTENSIONS = [...]
+
+# DOCUMENT_EXTENSIONS = [...]
+
+# Now every file can import the same configuration.
