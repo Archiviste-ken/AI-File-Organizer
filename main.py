d87b8620e33f124a8bdacb2782dcd1b60f2a4e42
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-27 01:11:03 +0530

    add main.py with initial project structure and responsibilities

---
+++ main.py
@@ -0,0 +1,39 @@
+# 📄 main.py
+
+# The Boss
+
+# Exactly like your previous projects.
+
+# Responsibilities:
+
+# ✔ Show Menu
+
+# ✔ Get User Input
+
+# ✔ Call Functions
+
+# ✔ Control Program Flow
+
+# Notice...
+
+# It never moves files.
+
+# It never creates folders.
+
+# It simply tells other files what to do.
+
+# Example flow:
+
+# User
+
+# ↓
+
+# Choose Folder
+
+# ↓
+
+# Organize Files
+
+# ↓
+
+# Show Success
