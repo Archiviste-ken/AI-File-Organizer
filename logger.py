47ff40bbbf35984fee1669f40f64586d27f3ac69
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-27 01:16:49 +0530

    add logger.py to implement action logging for better traceability

---
+++ logger.py
@@ -0,0 +1,35 @@
+# 📄 logger.py
+
+# Brand new concept.
+
+# Every action gets recorded.
+
+# Example:
+
+# 2026-07-28 14:21
+
+# Moved
+
+# photo.png
+
+# ↓
+
+# Images/
+
+# Later:
+
+# 2026-07-28 14:22
+
+# Moved
+
+# movie.mp4
+
+# ↓
+
+# Videos/
+
+# Why?
+
+# Real applications keep logs.
+
+# If something goes wrong, you can investigate what happened.
